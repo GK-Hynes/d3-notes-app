cdf364b7113a9a7dad190123acf70be96e990c3d
Gerard <gkhynes@gmail.com>
2018-02-11 16:55:08 +0000

    Overwrite Prettier formatting

--- app.js
+++ app.js
@@ -1,37 +1,48 @@
 var input = d3.select("input");
 var preview = d3.select(".preview");
 
+// prettier-ignore
 // New note
-d3.select("#new-note").on("submit", function() {
-  d3.event.preventDefault();
-  d3
-    .select("#notes")
-    .append("p")
-    .classed("note", true)
-    .text(input.property("value"));
-  input.property("value", "");
-  setPreview("");
-});
+d3.select("#new-note")
+    .on("submit", function() {
+      d3.event.preventDefault();
+      d3.select("#notes")
+        .append("p")
+          .classed("note", true)
+          .text(input.property("value"));
+      input.property("value", "");
+      setPreview("");
+    });
 
+// prettier-ignore
 // Clear notes
-d3.select(".remove").on("click", function() {
-  d3.selectAll(".note").remove();
-});
+d3.select(".remove")
+    .on("click", function() {
+      d3.selectAll(".note")
+        .remove();
+    });
 
+// prettier-ignore
 // Randomize font-size of notes
-d3.select(".lucky").on("click", function() {
-  d3.selectAll(".note").style("font-size", function() {
-    return Math.random() * 100 + "px";
+d3.select(".lucky")
+  .on("click", function() {
+    d3.selectAll(".note")
+        .style("font-size", function() {
+          return Math.random() * 100 + "px";
+        });
   });
-});
 
+// prettier-ignore
 // Listen for input
 input.on("input", function() {
   var note = d3.event.target.value;
   setPreview(note);
 });
 
+// prettier-ignore
 // Hide preview when empty
 function setPreview(val) {
-  preview.text(val).classed("hide", val === "");
+  preview
+    .text(val)
+    .classed("hide", val === "");
 }
